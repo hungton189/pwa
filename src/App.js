32c8f0f123f238452e03e588f882a378d7168c7d
hungton24hdev <hungton.24hdev@gmail.com>
2021-10-26 22:54:21 +0700

    Change content app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hưng Tôn</div>;
+  return <div className="App">React app use PWA</div>;
 }
 
 export default App;
